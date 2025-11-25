fbe831450f0fea02b9232aedb5c3d69218cb1ce1
israel-oye <ioyeboade@gmail.com>
2025-11-25 13:09:11 +0100

    add refresh implementation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,14 +8,29 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final provider = context.watch<ProductsProvider>();
-    return ListView.builder(
-      itemCount: provider.items.length,
-      itemBuilder: (ctx, idx){
-        Product currentProduct = provider.items[idx];
-        return ProductItem(product: currentProduct);
-      }
-      );
+  Widget build(BuildContext context) {   
+    
+
+    return Consumer<ProductsProvider>(
+      builder: (context, provider, _) {
+        if (provider.isLoading){
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return RefreshIndicator(
+          onRefresh: ()async{
+            provider.fetchProducts();
+          },
+          child: ListView.builder(
+            itemCount: provider.items.length,
+            itemBuilder: (ctx, idx) {
+              Product currentProduct = provider.items[idx];
+              return ProductItem(product: currentProduct);
+            },
+          ),
+        );
+      },
+    );
   }
 }
