50b2c07244e0aa7115261930cb7dd17d73087d75
israel-oye <ioyeboade@gmail.com>
2025-12-02 11:57:44 +0100

    Add product detail bottom sheet

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -6,14 +6,17 @@ import 'package:shop_app/providers/cart_provider.dart';
 class ProductItem extends StatelessWidget {
   final Product product;
 
-  const ProductItem({super.key, required this.product});
-
-  
+  const ProductItem({super.key, required this.product});  
 
   @override
   Widget build(BuildContext context) {
     final cartProvider = context.watch<CartProvider>();
     return ListTile(
+      onTap: (){
+        showModalBottomSheet(
+          context: context, 
+          builder: (ctx)=> ProductDetail(product: product));
+      },
       leading: CircleAvatar(backgroundColor: product.avatar,),
       title: Text(product.name),
       subtitle: Text('₦ ${product.price.toStringAsFixed(2)}'),
@@ -27,6 +30,39 @@ class ProductItem extends StatelessWidget {
           }
         }
       ),      
+    );
+  }
+}
+
+
+class ProductDetail extends StatelessWidget {
+  final Product product;
+  const ProductDetail({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: RadialGradient(colors: [
+                  product.avatar,
+                  product.avatar.withAlpha(150),
+                ])
+              ),
+            ),
+            SizedBox(height: 8,),
+            Text(product.name, style: TextStyle(fontSize: 24),)
+          ],
+        ),
+      ),
     );
   }
 }
